ea0c92b473610fd2383a2eea2fbc04290a000804
sos171809ajr <alvarojimenezro@hotmail.es>
2018-05-26 15:23:33 +0000

    a

---
+++ test/spanishUniversitiesTests/T00-API.js
@@ -0,0 +1,21 @@
+/*global expect*/
+/*global browser*/
+/*global element*/
+/*global by*/
+var newman = require('newman');
+var path = require('path');
+
+describe('API works', function() {
+    newman.run({
+        collection: require(path.join(process.cwd(), "test", "spanishUniversitiesTests",
+            "SOS1718-09-spanish-universities.postman_collection.json")),
+        reporters: "cli"
+
+    }, function(err) {
+        if (err)
+            throw err;
+        else
+            console.log("Newman collection run completed!");
+    });
+
+});
